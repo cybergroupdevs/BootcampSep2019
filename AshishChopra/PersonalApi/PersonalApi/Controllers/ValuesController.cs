08bbe88915f67885d9adfeb82a96fb35fcc74398
Ashish Chopra <ashish1194.cse16@chitkara.edu.in>
2019-09-19 18:44:54 +0530

    CRUD operations

---
+++ AshishChopra/PersonalApi/PersonalApi/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PersonalApi.Models;
+using System.Threading.Tasks;
+
+namespace PersonalApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        ashishContext obj = new ashishContext(); 
+        //ValuesController(ashishContext context)
+        //{
+        //    obj = context;
+        //}
+       
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+
+            var a = obj.Product.ToList();
+            return a ;
+
+        }
+        
+
+        // GET api/values/5
+        [Route("test")]
+        [HttpGet("{id}")]
+        public string Products(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Product value)
+        {
+            obj.Product.Add(value);
+            obj.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Product value)
+        {
+            Product put = obj.Product.FirstOrDefault(e =>e.Pid==id);
+            put.Pid = value.Pid;
+            put.Pname =value.Pname ;
+            obj.SaveChanges();
+         
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var a = obj.Product.Find(id);
+            obj.Product.Remove(a);
+            obj.SaveChanges();
+        }
+    }
+}
---
+++ AshishChopra/PersonalApi/PersonalApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonalApi.Models
+{
+    public partial class Product
+    {
+        public int Pid { get; set; }
+        public string Pname { get; set; }
+
+        public static implicit operator int(Product v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
