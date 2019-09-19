7e18271839131d5fb658c8881569c3cf56d775ed
Chandan <ch.cse16@chitkarauniversity.edu.in>
2019-09-19 18:21:07 +0530

    add apiproject

---
+++ chandan/Apiproject/Apiproject/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apiproject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apiproject.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        ChandanContext c = new ChandanContext();
+        [HttpGet]
+        public IEnumerable<Brands> Get()
+        {
+            var itm = c.Brands.ToList();
+            return itm;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Brands Get(int id)
+        {
+            var x = c.Brands.Find(id);
+            return x;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Brands value)
+        {
+            c.Brands.Add(value);
+            c.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Brands value)
+        {
+            var w = c.Brands.FirstOrDefault(e => e.Pid == id);
+            w.Pid = value.Pid;
+            w.Bid = value.Bid;
+            w.Bname = value.Bname;
+
+            c.SaveChanges();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            c.Brands.Remove(c.Brands.FirstOrDefault(e=>e.Pid == id));
+            c.SaveChanges();
+        }
+    }
+}
---
+++ chandan/Apiproject/Apiproject/Models/ChandanContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Apiproject.Models
+{
+    public partial class ChandanContext : DbContext
+    {
+        public ChandanContext()
+        {
+
+        }
+        public ChandanContext(DbContextOptions<ChandanContext> options):base(options)
+        {
+
+        }
+        public virtual DbSet<Brands> Brands { get; set; }
+        public virtual DbSet<Orders1> Orders1 { get; set; }
+        public virtual DbSet<Products1> Products1 { get; set; }
+        public virtual DbSet<Taxes> Taxes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            String str = @"Server=CYG348;Database=Chandan;Trusted_Connection=true;";
+            optionsBuilder.UseSqlServer(str);
+            base.OnConfiguring(optionsBuilder);
+           // if (!optionsBuilder.IsConfigured)
+           // {
+           //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+           //  optionsBuilder.UseSqlServer(@"Server=CYG348;Database=Chandan;Trusted_Connection=True;");
+           // }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brands>(entity =>
+            {
+                entity.HasKey(e => e.Pid);
+
+                entity.Property(e => e.Pid).ValueGeneratedNever();
+
+                entity.Property(e => e.Bname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Orders1>(entity =>
+            {
+                entity.HasKey(e => e.Pname);
+
+                entity.Property(e => e.Pname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Products1>(entity =>
+            {
+                entity.HasKey(e => e.Pid);
+
+                entity.Property(e => e.Pid).ValueGeneratedNever();
+
+                entity.Property(e => e.Pname)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Taxes>(entity =>
+            {
+                entity.HasKey(e => e.Pid);
+
+                entity.Property(e => e.Pid).ValueGeneratedNever();
+            });
+        }
+    }
+}
