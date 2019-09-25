7fd5060f49af3bfe3af761016d92a59346323446
Ganga Singh <ganga.singh@cybergindia.local>
2019-09-25 14:12:22 +0530

    auth changes

--- gangaSingh/Student-details/Student-Menu/Student-Menu/Program.cs
+++ gangaSingh/Student-details/Student-Menu/Student-Menu/Program.cs
@@ -29,7 +29,8 @@ namespace Student_Menu
                     try
                     {
 
-                        StreamReader sr = new StreamReader("E:/Student-details/Student-Menu/Text-file.txt");
+                        StreamReader sr = new StreamReader("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt");
+
 
                         line = sr.ReadLine();
 
@@ -57,46 +58,97 @@ namespace Student_Menu
                 case 2:
                     String inputLine;
 
-                    string path = @"E:/Student-details/Student-Menu/Text-file.txt";
+                    string path = @"E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt";
 
                     if (!File.Exists(path))
                     {
 
                         using (StreamWriter sw = File.CreateText(path))
                         {
+               
                             Console.WriteLine("Enter Student Details below : ");
                             inputLine = Console.ReadLine();
                             sw.WriteLine(inputLine);
                             sw.Close();
                         }
                     }
-
+                    String inputId, FirstName, LastName, City, State;
                     using (StreamWriter sw = File.AppendText(path))
                     {
-                        Console.WriteLine("Enter Student Details below : ");
-                        inputLine = Console.ReadLine();
-                        sw.WriteLine(inputLine);
+                        Console.WriteLine("Enter Student id : ");
+                        inputId = Console.ReadLine();
+                        //sw.WriteLine(inputId);
+                        Console.WriteLine("Enter Student Fisrt Name : ");
+                        FirstName = Console.ReadLine();
+                       // sw.WriteLine(FirstName);
+                        Console.WriteLine("Enter Student Last Name : ");
+                        LastName = Console.ReadLine();
+                       // sw.WriteLine(LastName,FirstName);
+                        Console.WriteLine("Enter Student City : ");
+                        City = Console.ReadLine();
+                        //sw.WriteLine(City);
+                        Console.WriteLine("Enter Student State : ");
+                        State = Console.ReadLine();
+                        
+                       
+                        String DummyLine = inputId+ " " + FirstName + " " + LastName + " " + City + " " + State;
+                        sw.WriteLine(DummyLine);
                         sw.Close();
                     }
                     break;
 
                 case 3:
-                    String OldText,
-                    NewInput;
-                    string text = File.ReadAllText("E:/Student-details/Student-Menu/Text-file.txt");
-                    Console.WriteLine("Enter the Student name to update : ");
+                    String OldText,IDstudent,NewInput;
+                    string text = File.ReadAllText("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt");
+                    Console.WriteLine("Enter the Student Id to update : ");
+                    IDstudent = Console.ReadLine();
+                    string[] lines = File.ReadAllLines(text);
+                    string[] words;
+
+                    foreach (string lineInFile in lines)
+                    {
+                        words = lineInFile.Split(' ');
+                    }
+                    // Console.WriteLine(lineInFile);
+                    Console.WriteLine("Enter the detail you want to update: 1 firstName 2 LastName 3 State 4 City ");
+                    int casesInput;
+                    casesInput = Convert.ToInt32(Console.ReadLine());
+                    switch (casesInput)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the new detail of the Student : ");
+                            NewInput = Console.ReadLine();
+                            // String lineTOChange = Array.Find(words, "1");
+                            // = NewInput;
+                           // string[] words1 = words;
+                            foreach (var word in words)
+                            {
+                                System.Console.WriteLine($"<{word}>");
+                            }
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                    }
+
+
+
+
+                    Console.WriteLine("Enter the Student Name to update : ");
                     OldText = Console.ReadLine();
-                    Console.WriteLine("Enter the new detail of the Student : ");
-                    NewInput = Console.ReadLine();
+                    
                     text = text.Replace(OldText, NewInput);
-                    File.WriteAllText("E:/Student-details/Student-Menu/Text-file.txt", text);
+                    File.WriteAllText("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt", text);
                     break;
 
                 case 4:
                     string tempFile = Path.GetTempFileName();
                     Console.Write("Input your text you want to delete: ");
                     var textTodelete = Console.ReadLine();
-                    using (var sr = new StreamReader("E:/Student-details/Student-Menu/Text-file.txt"))
+                    using (var sr = new StreamReader("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt"))
                     using (var sw = new StreamWriter(tempFile))
                     {
                         string lineByLine;
@@ -107,15 +159,15 @@ namespace Student_Menu
                         }
                     }
 
-                    File.Delete("E:/Student-details/Student-Menu/Text-file.txt");
-                    File.Move(tempFile, "E:/Student-details/Student-Menu/Text-file.txt");
+                    File.Delete("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt");
+                    File.Move(tempFile, "E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt");
                     break;
                 case 5:
                     Console.Write("Input your search text: ");
                     var textToSearch = Console.ReadLine();
                     List<List<string>> groups = new List<List<string>>();
                     List<string> current = null;
-                    foreach (var lineInLoop in File.ReadAllLines("E:/Student-details/Student-Menu/Text-file.txt"))
+                    foreach (var lineInLoop in File.ReadAllLines("E:/BootcampSep2019-master/BootcampSep2019/gangaSingh/Student-details/Student-Menu/Text-file.txt"))
                     {
                         if (lineInLoop.Contains(textToSearch) && current == null) current = new List<string>();
                         else if (lineInLoop.Contains(textToSearch) && current != null)
