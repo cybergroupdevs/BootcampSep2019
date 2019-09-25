72032555f7d49ee29f726c6504ccedf4d54d1b76
Ganga Singh <ganga.singh@cybergindia.local>
2019-09-25 17:17:39 +0530

    SignIn controller created, previously an error occured

---
+++ gangaSingh/Credentials/signup/Controllers/SignInController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using signup.Models;
+
+namespace signup.Controllers
+{
+    public class SignInController : Controller
+    {
+        dataContext obj = new dataContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("signin")]
+        public ActionResult post([FromBody]Userdetails userdata)
+        {
+            var row = obj.Userdetails.SingleOrDefault(r => r.Username == userdata.Username);
+            if (row.Username == userdata.Username && row.Password == userdata.Password)
+            {
+                return Ok("user Found");
+            }
+            return Unauthorized();
+          
+        }
+    }
+}
