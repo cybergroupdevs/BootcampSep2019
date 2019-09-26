2049f6bf404701851ba71ac7e455d854ce1141c8
Ganga Singh <ganga.singh@cybergindia.local>
2019-09-26 13:00:30 +0530

    Bcrypt functionallity is added

---
+++ gangaSingh/Credentials/signup/Models/Studentdetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace signup.Models
+{
+    public partial class Studentdetails
+    {
+        public string Username { get; set; }
+        public string Collegeid { get; set; }
+        public string Collegename { get; set; }
+        public string Email { get; set; }
+
+        public Userdetails UsernameNavigation { get; set; }
+    }
+}
