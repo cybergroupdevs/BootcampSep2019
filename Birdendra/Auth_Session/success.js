973517696e6cb131e2e6805e657fea21671cb6ab
birendra-bit <bhujelbirendra3@gmail.com>
2019-10-07 21:34:53 +0530

    auth token send from success.html page for validation

--- Birdendra/Auth_Session/success.js
+++ Birdendra/Auth_Session/success.js
@@ -1,21 +1,18 @@
 $(document).ready( function(){
     $('#btn').click( function(){
-        // console.log(user_id+' '+pwd);
-       
-        console.log(window.localStorage.getItem('token'));
+        console.log("button click");
+        //get the token using local storage and append Beraer infront it and assign authorization key
+        var tkn =window.localStorage.getItem('token');
         $.ajax("http://localhost:58530/values/Get",{
             type:"GET",
-            headers: {"Authorization": window.localStorage.getItem('token')},
+            headers: {"Authorization": `Bearer ${tkn}`},
             dataType: "json",
             contentType: "application/json",
             success:function(data, status){
-
-                // window.open("success.html");
                 $("table tbody").empty();
                     var th ="<tr><th>SL_NO</th><th>Name</th><th>Email</th><th>College Name</th><th>College Id</th></tr>";
                     $("table tbody").append(th);    
                 for ( var i = 0 ; i < data.length; i++ ){
-                    // $('#table').append('<tr><td>hello</td></tr>');
                     var tbl = "<tr><td>"+i+"</td><td>"+data[i].name+"</td><td>"+data[i].userId+"</td><td>"+data[i].colName+"</td><td>"+data[i].colId+"</td></tr>";
                     $("table tbody").append(tbl);
                 }
