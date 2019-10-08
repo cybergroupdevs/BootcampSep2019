3684cc1ac38a423dbd6bdf2bbdbc9e239e9f3ebc
Ganga Singh <gangasingh.cse16@chitkarauniversity.edu.in>
2019-10-08 16:55:19 +0530

    Created schema for userauth

---
+++ gangaSingh/Reddit/rest-apis-express/schema/googleauthdetails.js
@@ -0,0 +1,6 @@
+var googleauthdetailsschema = new mongoose.Schema({
+    email     : String,
+    imageurl    : String,
+});
+
+var googleauthdetailsmodel = mongoose.model('googleauthdetailsmodel', googleauthdetailsschema);
