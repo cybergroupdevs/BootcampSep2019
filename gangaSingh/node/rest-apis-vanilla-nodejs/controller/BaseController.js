e384e78692da6938d9eaa3afa0715ec351e93b99
Ganga Singh <gangasingh.cse16@chitkarauniversity.edu.in>
2019-10-15 13:45:52 +0530

    Added code of file operations

--- gangaSingh/node/rest-apis-vanilla-nodejs/controller/BaseController.js
+++ gangaSingh/node/rest-apis-vanilla-nodejs/controller/BaseController.js
@@ -1,10 +1,17 @@
-const model = require("../mongoose").user;
+var path = require("path");
+var fs = require('fs');
 class BaseController {
-    async create(req, res) {
+    constructor(model) {
+        this.users = [];
+    //  this.model = model;
+    }
+     // Create a file to write a information of client
+     async create(req, res) {
+        try {
             req.on("end", () => {
-                const userInfoFromClient = req.body;
-                const client = this.model(userInfoFromClient);
-                model.create(client);
+                const user = req.body;
+                this.users.push(user);
+                writeFile(user);
                 const response = {
                     status: 200,
                     statusText: "OK",
@@ -14,22 +21,59 @@ class BaseController {
                 res.setHeader("content-type", "application/json");
                 res.end(JSON.stringify(response));
             })
+        } catch (err) {
+            console.error(err);
+        }
     }
+    // Read a file to get client data
+    async read(res) {
+        //return await this.model.find();
+        try {
+            readFile()
+                .then(data => {
+                    this.users = JSON.parse(data);
 
-    async read(req,res) {
-        req.on("end", () => {
-          let userInfoFromClient = req.body;
-                  let response = model.findOne(userInfoFromClient.email)
-                  res.setHeader("content-type", "application/json");
-                  res.end(JSON.stringify(response));
+                    const response = {
+                        total: this.users.length,
+                        data: this.users
+                    };
+
+                    res.setHeader("content-type", "application/json");
+                    res.end(JSON.stringify(response));
                 })
-    }
+                .catch(err => {
+                    const response = {
+                        error: err
+                    };
 
+                    res.setHeader("content-type", "application/json");
+                    res.end(JSON.stringify(response));
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    }
     async update(req, res) {
-            req.on("end", () => {        
-                const id = Number.parseInt(req.body._id);
-                const user = req.body; 
-                model.update({_id:id}, { $set: { name: req.body.name } })
+        try {
+            req.on("end", () => {
+                let query = req.query;
+                // console.log("Queru om controller");
+                const age = Number.parseInt(query.age); // age from postman query
+                const user = req.body; // from postman
+                readFile()
+                    .then(data => {
+                        this.users = JSON.parse(data);
+                        if (age == this.users.age) {
+                            this.users.name = user;
+                            user.age = age;
+                            writeFile(user);
+                        }
+                        //const response = { total: this.users.length, data: this.users };
+                    }).catch(err => {
+                        const response = {
+                            error: err
+                        };
+                      });
                 const response = {
                     status: 200,
                     statusText: "OK",
@@ -39,13 +83,32 @@ class BaseController {
                 res.setHeader("content-type", "application/json");
                 res.end(JSON.stringify(response));
             });
+        } catch (err) {
+            console.error(err);
+        }
     }
 
+    //delete client details in file
     async delete(req,res) {
+        try {
           req.on("end", () => {
-            let body = req.body;
-            const _id = Number.parseInt(body._id); 
-            model.remove(_id)
+            let query = req.query;
+            const age = Number.parseInt(query.age); // age from postman query
+
+            readFile()
+                .then(data => {
+                    this.users = JSON.parse(data);
+                    if (age == this.users.age) {
+                      console.log("Age in delete function");
+                        delete this.users.name;
+                        delete this.users.age;
+                        writeFile(this.users);
+                    }
+                }).catch(err => {
+                    const response = {
+                        error: err
+                    };
+                  });
             const response = {
                 status: 200,
                 statusText: "OK",
@@ -55,7 +118,24 @@ class BaseController {
             res.setHeader("content-type", "application/json");
             res.end(JSON.stringify(response));
         });
+
+    } catch (err) {
+        console.error(err);
     }
 }
 
-exports.BaseController = BaseController;
+}
+
+// Functions to read and write data in file .... !! data means user details 
+
+function readFile() {
+    const dbPath = path.join(__dirname, "../db", "users.json");
+    return fs.promises.readFile(dbPath, "utf-8");
+}
+
+function writeFile(data) {
+    console.log("In WriteFile:", data);
+    const dbPath = path.join(__dirname, "../db", "users.json");
+    return fs.promises.writeFile(dbPath, JSON.stringify(data));
+}
+exports.BaseController = BaseController; 
